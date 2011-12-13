666876e1735cfbfb61aedd04ad643d1b5448055e
Stephen Zabel <szabel@q2software.com>
2011-12-13 17:41:29 -0600

    Not installing __init__

--- api_site/__init__.py
+++ api_site/__init__.py
@@ -7,6 +7,6 @@ __authors__=[
     'Stephen Zabel <sjzabel@gmail.com>'
 ]
 
-#from api_site.sites import site
-#from api_site.models import ModelApi
+from api_site.sites import site
+from api_site.models import ModelApi
 
